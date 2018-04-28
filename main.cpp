701c6cd584baae92b9ea40ff03db3d2b99b6239e
FreddeG <fredrik.ano@gmail.com>
2018-04-28 17:16:29 +0200

    asdadaad

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <functional>
+
+int main(int argc, char const *argv[])
+{
+	  std::cout << "Hello World!" << std::endl << "wtf" << std::endl << "Test :" << argc << std::endl;
+	  //std::cout << "Test :" << argc << std::endl;
+	  //std::cout << argv[0] << std::endl;
+	  for ( int i = 0; i<argc; i++)
+	  {
+
+	  		  std::cout << argv[i] << std::endl;
+	  }
+
+	return 0;
+}
